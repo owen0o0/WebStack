e509c8095d3bea22c85faae8ba18430fcd41d175
owen <32561126+owen0o0@users.noreply.github.com>
2024-07-30 17:38:48 +0800

    更新天气api

--- templates/header-banner.php
+++ templates/header-banner.php
@@ -6,8 +6,8 @@
  * @Author URI: https://www.iowen.cn/
  * @Date: 2019-02-22 21:26:02
  * @LastEditors: iowen
- * @LastEditTime: 2022-07-25 18:11:27
- * @FilePath: \WebStack\templates\header-banner.php
+ * @LastEditTime: 2024-07-30 17:31:38
+ * @FilePath: /WebStack/templates/header-banner.php
  * @Description: 
  */
 if ( ! defined( 'ABSPATH' ) ) { exit; }  ?>
@@ -22,8 +22,8 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }  ?>
         <!-- 天气 -->
         <li>
           <div id="he-plugin-simple"></div>
-          <script>WIDGET = {CONFIG: {"modules": "12034","background": 5,"tmpColor": "aaa","tmpSize": 16,"cityColor": "aaa","citySize": 16,"aqiSize": 16,"weatherIconSize": 24,"alertIconSize": 18,"padding": "30px 10px 30px 10px","shadow": "1","language": "auto","borderRadius": 5,"fixed": "false","vertical": "middle","horizontal": "left","key": "a922adf8928b4ac1ae7a31ae7375e191"}}</script>
-          <script src="https://widget.heweather.net/simple/static/js/he-simple-common.js?v=1.1"></script>
+          <script>(function(T,h,i,n,k,P,a,g,e){g=function(){P=h.createElement(i);a=h.getElementsByTagName(i)[0];P.src=k;P.charset="utf-8";P.async=1;a.parentNode.insertBefore(P,a)};T["ThinkPageWeatherWidgetObject"]=n;T[n]||(T[n]=function(){(T[n].q=T[n].q||[]).push(arguments)});T[n].l=+new Date();if(T.attachEvent){T.attachEvent("onload",g)}else{T.addEventListener("load",g,false)}}(window,document,"script","tpwidget","//widget.seniverse.com/widget/chameleon.js"))</script>
+          <script>tpwidget("init",{"flavor": "slim","location": "WX4FBXXFKE4F","geolocation": "enabled","language": "zh-chs","unit": "c","theme": "chameleon","container": "he-plugin-simple","bubble": "enabled","alarmType": "badge","color": "#999999","uid": "UD5EFC1165","hash": "2ee497836a31c599f67099ec09b0ef62"});tpwidget("show");</script>
         </li>
         <!-- 天气 end -->
       </ul>
